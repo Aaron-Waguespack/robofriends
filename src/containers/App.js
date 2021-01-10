71e195969b4223a429b054b72813ed8ce57a2ed3
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2021-01-10 13:35:31 -0700

    updated for hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,41 @@
-import React, { Component, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component{
-	constructor() {
-		super()
-		this.state={
-			robots:[],
-			searchfield:""
-		}
-	}
+function App () {
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		  .then(response=> response.json())
-		  .then(users => this.setState({robots: users}));
-	}
+		const [robots, setRobot] = useState([])
+		const [searchfield, setSearchfield] = useState('');
 
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
-		}
+		useEffect(()=> {
+			fetch('https://jsonplaceholder.typicode.com/users')
+		  		.then(response=> response.json())
+				.then(users => setRobot(users));
+		});
 
-	render(){
-		const {robots, searchfield} = this.state;
+		const onSearchChange =(event)=> {
+			setSearchfield(event.target.value)
+			};
+
 		const filteredRobots = robots.filter(robot=>{
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
+
 		if (robots.length ===0) {
 			return <h1>Loading</h1>
 		} else {
 		  return (
 			<div className='tc'>
 				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<CardList robots ={filteredRobots}/>
 				</Scroll>
 			</div>
 		  );
 		};
-	};
 };
 
 export default App;
